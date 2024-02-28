d28f38a89d49c512fcf346f8793764aa679a4815
Spoorthinbhat <spoorthiullal4@gmail.com>
2024-02-28 23:50:22 +0530

    changed the checkboxes to button

--- todo.js
+++ todo.js
@@ -13,29 +13,36 @@ function add() {
   let checkid = "c_" + Date.now();
   document.getElementById("work").innerHTML +=
     "<div class='point'>" +
-    "<input type='checkbox' id='" +
+    "<button id='sent'+ id='" +
     checkid +
     "' onclick='strike(this)'>" +
     line +
     "<br>" +
+    "</button>" +
     "</div>";
+  // "<div class='point'>" +
+  // "<input type='checkbox' id='" +
+  // checkid +
+  // "' onclick='strike(this)'>" +
+  // line +
+  // "<br>" +
+  // "</div>";
   document.getElementById("insert").value = "";
   comp();
 }
 function strike(a) {
-  if (a.checked) {
-    a.parentNode.style.textDecoration = "line-through";
-    a.parentNode.style.backgroundColor = "#FFFFFF";
-    completed++;
-    comp();
-  } else {
-    a.parentNode.style.textDecoration = "none";
-    a.parentNode.style.backgroundColor = "#68bbe3";
+  if (a.style.textDecoration === "line-through") {
+    a.style.textDecoration = "none";
+    a.style.backgroundColor = "#68bbe3";
     completed--;
-    comp();
+  } else {
+    a.style.textDecoration = "line-through";
+    a.style.backgroundColor = "#FFFFFF";
+    completed++;
   }
+  comp();
 }
-
+X;
 function comp() {
   document.getElementById("status").innerText =
     "Completed : " + "\n" + completed;
